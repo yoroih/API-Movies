da8f668abc1c0a7b0fc0acfdd9265fa88eec01c1
yoroih <roycristhopherqp@gmail.com>
2023-03-03 18:13:23 -0500

    feat: :sparkles: add app user

--- movies/urls.py
+++ movies/urls.py
@@ -20,5 +20,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movie/', include('movie.urls')),
+    path('user/', include('user.urls')),
     path('swagger/', schema_view.with_ui("swagger", cache_timeout=0), name="swagger_docs")
 ]
